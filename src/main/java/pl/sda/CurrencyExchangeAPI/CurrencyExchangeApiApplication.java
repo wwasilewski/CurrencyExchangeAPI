90ce0ee005c8485b040bcbbf26801e11644d7d19
Wojciech <w.wasilewski@gmail.com>
2022-04-12 10:13:10 +0200

    start

--- src/main/java/pl/sda/CurrencyExchangeAPI/CurrencyExchangeApiApplication.java
+++ src/main/java/pl/sda/CurrencyExchangeAPI/CurrencyExchangeApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyExchangeApiApplication {
 
+	// komentarz dla Kasi
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeApiApplication.class, args);
 	}
