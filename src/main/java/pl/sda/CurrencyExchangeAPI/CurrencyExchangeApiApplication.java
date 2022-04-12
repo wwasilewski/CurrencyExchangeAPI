f8408efdb8ad23f3acbd2344ff8018e72ede6c5f
Wojciech <w.wasilewski@gmail.com>
2022-04-12 10:05:36 +0200

    Initial commit

---
+++ src/main/java/pl/sda/CurrencyExchangeAPI/CurrencyExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.CurrencyExchangeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/CurrencyExchangeAPI/CurrencyExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.CurrencyExchangeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
