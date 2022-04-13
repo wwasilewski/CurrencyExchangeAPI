237542936c240e8befc5b177068f3185b14ddbb0
Kasia <sawicka.katarzyna91@gmail.com>
2022-04-13 11:48:16 +0200

    created entity from CurrencyRate and add API reader for history values

--- src/main/java/pl/sda/CurrencyExchangeAPI/model/CurrencyRate.java
+++ src/main/java/pl/sda/CurrencyExchangeAPI/model/CurrencyRate.java
@@ -1,16 +1,26 @@
 package pl.sda.CurrencyExchangeAPI.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "currency")
 public class CurrencyRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "base", nullable = false)
     private String base;
+    @Column(name = "target", nullable = false)
     private String target;
+    @Column(name = "rate", nullable = false)
     private double rate;
+    @Column(name = "date", nullable = false)
     private LocalDateTime date;
 }
