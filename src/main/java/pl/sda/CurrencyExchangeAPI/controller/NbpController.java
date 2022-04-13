c36e86807901d9873c52a3986505740c55914482
Kasia <sawicka.katarzyna91@gmail.com>
2022-04-13 10:49:29 +0200

    created model CurrencyRate.java

--- src/main/java/pl/sda/CurrencyExchangeAPI/controller/NbpController.java
+++ src/main/java/pl/sda/CurrencyExchangeAPI/controller/NbpController.java
@@ -2,7 +2,6 @@ package pl.sda.CurrencyExchangeAPI.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
