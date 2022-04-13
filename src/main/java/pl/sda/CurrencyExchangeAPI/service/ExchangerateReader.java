cb3157f2414927c6222e341142dc46bd82bfd015
Wojciech <w.wasilewski@gmail.com>
2022-04-13 15:22:17 +0200

    add CurrencyExchangeService methods

--- src/main/java/pl/sda/CurrencyExchangeAPI/service/ExchangerateReader.java
+++ src/main/java/pl/sda/CurrencyExchangeAPI/service/ExchangerateReader.java
@@ -2,6 +2,7 @@ package pl.sda.CurrencyExchangeAPI.service;
 
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import pl.sda.CurrencyExchangeAPI.model.CurrencyRate;
 
 import java.io.IOException;
@@ -13,12 +14,12 @@ import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 
 @Slf4j
+@Service
 public class ExchangerateReader {
 
 
-
     public String createAPICallForLatestRate(String base, String symbol) {
-    return "https://api.exchangerate.host/latest?base=" + base + "&symbols=" + symbol;
+        return "https://api.exchangerate.host/latest?base=" + base + "&symbols=" + symbol;
     }
 
     public String createAPICallForHistoryRate(String base, String symbol, LocalDateTime date) {
