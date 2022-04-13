8f0fc617c1da802822c5ba2ff7aaa577fea4f0a5
Kasia <sawicka.katarzyna91@gmail.com>
2022-04-13 12:15:09 +0200

    created repository

---
+++ src/main/java/pl/sda/CurrencyExchangeAPI/repository/CurrencyRepository.java
@@ -0,0 +1,9 @@
+package pl.sda.CurrencyExchangeAPI.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRepository extends CrudRepository {
+
+}
