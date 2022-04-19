2e477c1e97d3aeb12751d7c83e33f954a8fbf5ff
Kasia <sawicka.katarzyna91@gmail.com>
2022-04-19 14:03:25 +0200

    wrong currency validation

--- src/main/java/pl/sda/CurrencyExchangeAPI/service/CurrencyExchangeService.java
+++ src/main/java/pl/sda/CurrencyExchangeAPI/service/CurrencyExchangeService.java
@@ -7,6 +7,7 @@ import pl.sda.CurrencyExchangeAPI.model.CurrencyRate;
 import pl.sda.CurrencyExchangeAPI.model.RateValue;
 import pl.sda.CurrencyExchangeAPI.repository.CurrencyRepository;
 
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -19,21 +20,18 @@ public class CurrencyExchangeService {
 
     private final CurrencyRepository currencyRepository;
     private final CurrencyMapper currencyMapper;
-    private final ExchangerateReader exchangerateReader;
-    private final CurrencyExistValidation currencyExistValidation;
+    private final ExchangeRateReader exchangerateReader;
 
     @Autowired
     public CurrencyExchangeService(CurrencyRepository currencyRepository, CurrencyMapper currencyMapper,
-                                   ExchangerateReader exchangerateReader, CurrencyExistValidation currencyExistValidation) {
+                                   ExchangeRateReader exchangerateReader) {
         this.currencyRepository = currencyRepository;
         this.currencyMapper = currencyMapper;
         this.exchangerateReader = exchangerateReader;
-        this.currencyExistValidation = currencyExistValidation;
     }
 
 
     public CurrencyRateDto getLatestCurrencyRate(String base, String target) {
-        if(currencyExistValidation.isCurrencyExisting(base) && currencyExistValidation.isCurrencyExisting(target)) {
         CurrencyRate currencyRate = new CurrencyRate();
         currencyRate.setBase(base.toUpperCase());
         currencyRate.setTarget(target.toUpperCase());
@@ -52,8 +50,6 @@ public class CurrencyExchangeService {
             currencyRate.setRate(rateValue.getRates().get(target.toUpperCase()));
             currencyRepository.save(currencyRate);
             return currencyMapper.map(currencyRate);
-        }} else {
-            throw
         }
 
     }
